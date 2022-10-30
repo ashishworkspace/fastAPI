9720f3856e26c06f181502f105c6c257f633ba89
Ashish Kumar <ashishizofficial@gmail.com>
2022-10-30 14:16:45 +0530

    [get, post added]

---
+++ main.py
@@ -0,0 +1,34 @@
+import random
+from typing import Optional
+from fastapi import FastAPI, status
+from pydantic import BaseModel
+
+app = FastAPI()
+
+allPost = []
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: Optional[bool] = True
+    rating: Optional[int]
+
+# POST
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def createPost(post: Post):
+    id = random.randrange(0, 1000000)
+    postDict = post.dict()
+    postDict['id'] = id
+    allPost.append(post)
+    return {
+        "msg": "Post created"
+    }
+
+
+# GET
+@app.get("/posts", status_code=status.HTTP_200_OK)
+def getPost():
+    return allPost
